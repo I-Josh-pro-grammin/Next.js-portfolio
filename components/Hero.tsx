2b964549de488edfb7a00f4fd18264e74760fd5a
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-05-09 23:30:44 +0200

    Made a few changes to the styles of some files

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,61 +1,76 @@
-import React from 'react'
-import { Spotlight } from './ui/Spotlight'
-import { cn } from "@/lib/utils";
-import { TextGenerateEffect } from './ui/TextGenerateEffect';
-import MagicButton from './ui/MagicButton';
-import { FaLocationArrow } from 'react-icons/fa';
-import Link from 'next/link';
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className='pb-26 pt-36'>
-      <div className='overflow-hidden'>
-        <Spotlight className='top-10 left-0 md:-left-32 opacity-50 md:top-20 h-[200vh] w-[80vw]' fill='white' />
-        <Spotlight className='top-5 left-full w-[90vw] h-[80vh] sm:top-50 md:top-10' fill='blue' />
-        <Spotlight className='top-10 left-50 h-[200vh] w-[70vw] sm:top-50 md:top-1 ' fill='purple' />
+    <div className="pb-20 pt-36">
+      {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-        <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-[#000308] absolute top-0 left-0">
-          <div
-            className={cn(
-              "absolute inset-0",
-              "[background-size:40px_40px]",
-              "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-              "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
-            )}
-          />
+
+      {/**
+       *  UI: grid
+       *  change bg color to bg-black-100 and reduce grid color from
+       *  0.2 to 0.03
+       */}
+      <div
+        className="h-screen w-full bg-black dark:bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
         {/* Radial gradient for the container to give a faded look */}
-          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-          <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+        <div
+          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
           </p>
-        </div>
-        <div className='flex justify-center relative my-20 z-10'>
-          <div className='max-w-[90vw] md:max-w-2xl lg:max-w-[80vw] flex flex-col items-center justify-center'>
-            <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80' >
-              Dynamic Web Magic with Next.js
-            </h2>
 
-            {/* <TextGenerateEffect 
-               className=""
-               words = "Transforming Concepts into Seamless User Experiences"
-            /> */}
+          {/**
+           *  Link: https://ui.aceternity.com/components/text-generate-effect
+           *
+           *  change md:text-6xl, add more responsive code
+           */}
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
 
-            <h1 className='text-center font-semibold text-10 md:text-5xl mb-10 text-gray-100 lg:text-8xl'>Transforming Concepts into Seamless User Experiences</h1>
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+          </p>
 
-            <p className="text-center md:tracking-wider mb-4 text-gray-600 text-sm md:text-lg lg:text-xl ">
-              Hi, I&apos;m Joshua. A Full-Stack Developer with a passion for MERN Stack, NEST.JS, NEXT.JS and TAILWINDCSS 
-            </p>
-
-            <Link href="#about">
-              <MagicButton
-                title="Explore my work"
-                icon={<FaLocationArrow />}
-                position='right'
-              />
-            </Link>
-          </div>
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
